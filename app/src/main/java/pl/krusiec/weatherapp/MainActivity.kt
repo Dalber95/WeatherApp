d64b840457a372ff3a3d12e15f8b8a735b74cbd4
Kamil <krusiec.kamil82@gmail.com>
2020-06-20 12:00:02 +0200

    Changing The UI Depending On The Weather Data From The API

--- app/src/main/java/pl/krusiec/weatherapp/MainActivity.kt
+++ app/src/main/java/pl/krusiec/weatherapp/MainActivity.kt
@@ -197,6 +197,23 @@ class MainActivity : AppCompatActivity() {
             tv_speed.text = weatherList.wind.speed.toString()
             tv_name.text = weatherList.name
             tv_country.text = weatherList.sys.country
+
+            when(weatherList.weather[i].icon){
+                "01d" -> iv_main.setImageResource(R.drawable.sunny)
+                "02d" -> iv_main.setImageResource(R.drawable.cloud)
+                "03d" -> iv_main.setImageResource(R.drawable.cloud)
+                "04d" -> iv_main.setImageResource(R.drawable.cloud)
+                "04n" -> iv_main.setImageResource(R.drawable.cloud)
+                "10d" -> iv_main.setImageResource(R.drawable.rain)
+                "11d" -> iv_main.setImageResource(R.drawable.storm)
+                "13d" -> iv_main.setImageResource(R.drawable.snowflake)
+                "01n" -> iv_main.setImageResource(R.drawable.cloud)
+                "02n" -> iv_main.setImageResource(R.drawable.cloud)
+                "03n" -> iv_main.setImageResource(R.drawable.cloud)
+                "10n" -> iv_main.setImageResource(R.drawable.cloud)
+                "11n" -> iv_main.setImageResource(R.drawable.rain)
+                "13n" -> iv_main.setImageResource(R.drawable.snowflake)
+            }
         }
     }
 
